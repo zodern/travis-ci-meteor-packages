c6091e80e039a83e794698841ca21903cfb0c2ee
zodern <zodern@users.noreply.github.com>
2015-05-13 12:02:43 -0500

    use fork's files

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #configuring the system
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/start_test.js
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
+wget https://raw.github.com/zodern/travis-ci-meteor-packages/master/Makefile
+wget https://raw.github.com/zodern/travis-ci-meteor-packages/master/start_test.js
+wget https://raw.github.com/zodern/travis-ci-meteor-packages/master/phantom_runner.js
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
